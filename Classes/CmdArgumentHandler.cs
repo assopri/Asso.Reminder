bd724b59834df022cc2f720e8d98eb65a925c314
assopri <i4G4fyaa>
2021-06-14 19:44:53 +0300

    first (just label, command line, escape to close)

---
+++ Classes/CmdArgumentHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Asso.Reminder
+{
+    public static class CmdArgumentHandler
+    {
+        public static bool listContainsStr(List<string> list, string str, bool ignoreCase = true)
+        {
+            if (!ignoreCase) return list.Contains(str);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = list[i].ToLower();
+            }
+
+            str = str.ToLower();
+
+            return list.Contains(str);
+        }
+
+        public static bool arContainsStr(string[] ar, string str, bool ignoreCase = true)
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (String.Compare(str, ar[i], ignoreCase) == 0) return true;
+            }
+            return false;
+        }
+        //
+        public static string getCmdArgValue(string[] ar, string arg)
+        {
+            for (int i = 0; i < ar.Length; i++)
+            {
+                Match m = Regex.Match(ar[i], arg + "=(.*?)$");
+
+                if (m.Groups[1].Value.Trim() == "") continue;
+
+                return m.Groups[1].Value.Trim();
+            }
+
+            return "";
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asso.Reminder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] args = Environment.GetCommandLineArgs();
+            labelControl1.Text = CmdArgumentHandler.getCmdArgValue(args, "label");
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
